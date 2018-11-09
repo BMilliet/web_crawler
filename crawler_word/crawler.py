892af2f56d1e3bf1ea058f7d63e56f4c86d886b0
BMilliet <brunomilliet89@gmail.com>
2018-11-09 20:20:58 -0200

    isolando funcoes

--- crawler_word/crawler.py
+++ crawler_word/crawler.py
@@ -11,7 +11,6 @@
 """
 
 import urllib.request
-import urllib.error
 import json
 
 """
@@ -19,22 +18,20 @@ import json
     as URLs deve estar colocadas uma em cada linha.
 """
 
-fileFound = False
-try:
-    lista = open('URL_list.txt')
+def busca_lista(list):
     try:
-        urls = lista.read().split('\n')
-        fileFound = True
-    finally: lista.close()
+        lista = open(list)
+        try:
+            urls = lista.read().split('\n')
+        finally: lista.close()
 
-except FileNotFoundError:
+    except FileNotFoundError:
         print('Arquivo de lista não encontrado')
 
+    return urls
 
-"""A palavra buscada é colocada como um input pelo usuario ao executar o programa"""
 
-if(fileFound):
-    palavra = input('Qual palavra deseja procurar? ');
+def conta_palavra(urls,palavra):
     results = []
 
     for i in range(len(urls)-1):
@@ -44,16 +41,27 @@ if(fileFound):
         data = resp.read()
         html = data.decode("UTF-8")
         cont = html.count(palavra)
+
         result = json.dumps({'Url': urls[i], 'codigo': codigo, 'encontradas': cont})
         results.append(result)
 
+    return results
 
-"""
-   Os resultados contem a URL, o codigo de resposta e o numero de palavras encontradas
-"""
+
+def apresenta_relatorio(results):
     for i in range(len(results)):
         resp = json.loads(results[i])
         print(resp['encontradas'])
 
+"""Execução do programa"""
 
-print('\nFim')
+urls = busca_lista('URL_list.txt')
+
+"""A palavra buscada é colocada como um input pelo usuario ao executar o programa"""
+
+palavra = input('Qual palavra deseja procurar? ');
+
+results = conta_palavra(urls,palavra)
+apresenta_relatorio(results)
+
+print('Fim')
